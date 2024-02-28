05da257549c2fb3d5a06924912d35359b1c92931
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-02-28 18:35:30 +0530

    more styles added

--- components/article.jsx
+++ components/article.jsx
@@ -5,10 +5,10 @@ export function Article() {
         <>
         <div className="articlediv">
             <p><b>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</b></p>
-            <p>CHEMICALS & PROCESS <span style={{backgroundColor:'red'}}>|</span> POWER <span style={{backgroundColor:'red'}}>|</span> WATER & WASTE WATER <span style={{backgroundColor:'red'}}>|</span> OILS &
-                 GAS <span style={{backgroundColor:'red'}}>|</span> PHARMA <span style={{backgroundColor:'red'}}>|</span> SUGARS & DISTILLERIES <span style={{backgroundColor:'red'}}>|</span> PAPER & PULP <span style={{backgroundColor:'red'}}>|</span> MARINE & DEFENCE METAL
-                 & MINING <span style={{backgroundColor:'red'}}>|</span> FOOD & BEVERAGE <span style={{backgroundColor:'red'}}>|</span> PETROCHEMICAL & REFINERIES <span style={{backgroundColor:'red'}}>|</span> SOLAR <span style={{backgroundColor:'red'}}>|</span> BUILDING <span style={{backgroundColor:'red'}}>|</span> HVAC <span style={{backgroundColor:'red'}}>|</span>
-                 FIRE FIGHTING <span style={{backgroundColor:'red'}}>|</span> AGRICULTURE & RESIDENTIAL  </p>
+            <p>CHEMICALS & PROCESS <span style={{color:'red'}}>|</span> POWER <span style={{color:'red'}}>|</span> WATER & WASTE WATER <span style={{color:'red'}}>|</span> OILS &
+                 GAS <span style={{color:'red'}}>|</span> PHARMA <span style={{color:'red'}}>|</span> SUGARS & DISTILLERIES <span style={{color:'red'}}>|</span> PAPER & PULP <span style={{color:'red'}}>|</span> MARINE & DEFENCE METAL
+                 & MINING <span style={{color:'red'}}>|</span> FOOD & BEVERAGE <span style={{color:'red'}}>|</span> PETROCHEMICAL & REFINERIES <span style={{color:'red'}}>|</span> SOLAR <span style={{color:'red'}}>|</span> BUILDING <span style={{color:'red'}}>|</span> HVAC <span style={{color:'red'}}>|</span> FIRE 
+                 FIGHTING <span style={{color:'red'}}>|</span> AGRICULTURE & RESIDENTIAL  </p>
         </div>
           
             
